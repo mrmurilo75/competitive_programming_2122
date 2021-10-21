b8656e1cb6fa9d37fa50cda7bc063171d818e028
Murilo Rosa <mrmurilo75@gmail.com>
2021-10-21 22:19:10 +0100

    ps00 - kattis different - done

--- ps00/kattis_different.cpp
+++ ps00/kattis_different.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main(){
 	long a, b;
-	cin >> a; cin >> b;
-
-	cout << abs(a - b) << endl;
-
+	cin >> a >> b;
+	while(!cin.eof()){
+		cout << abs(a - b) << endl;
+		cin >> a >> b;
+	}
 }
 
