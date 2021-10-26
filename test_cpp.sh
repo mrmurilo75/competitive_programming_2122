284bfbce51658a847f66aa7bb89e357c01c331ff
Murilo Rosa <mrmurilo75@gmail.com>
2021-10-26 21:35:51 +0100

    / - update test_cpp

--- test_cpp.sh
+++ test_cpp.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-g++ -Wall $1 -o a.o && ./a.o < 'in.txt'
+g++ -Wall $1 -o a.o && ./a.o < 'in.txt' > 'out.txt' 
+if ls | grep 'exp_out.txt'; then 
+	vimdiff 'out.txt' 'exp_out.txt'
+else
+	cat 'out.txt'
+fi
 
