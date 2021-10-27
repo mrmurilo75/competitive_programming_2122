13d758725e4900621a821a963884d11485aaa6e8
mrmurilo75 <mrmurilo75@gmail.com>
2021-10-27 20:30:46 +0100

    ps01 - uva 11572 - in progress

--- ps01/uva_11572.cpp
+++ ps01/uva_11572.cpp
@@ -10,23 +10,23 @@ int main(){
 	while(cases--) {
 		int n;
 		cin >> n;
-		int sf[n], beginning = 0, size = 1, maximum = 1;
+		int sf[n + 1], beginning = 1, size = 0, maximum = 0;
+		sf[0] = 0;
 		map<int, int> last_pos;
-		cin >> sf[0];
-		last_pos[sf[0]] = 0;
-		for(int i = 1; i < n; i++) {
+		for(int i = 1; i < n +1; i++) {
 			cin >> sf[i];
-			if(!last_pos[sf[i]]) {
+			if(!last_pos[sf[i]]) { // aka last_pos of sf[i] is 0 (NULL) so its new
 				last_pos[sf[i]] = i;
 				size++;
-			} else {
+			} else if (last_pos[sf[i]] >= beginning) { // if sf[i] not new && last_pos of sf[i] is being accounted 
+				if(size > maximum)
+					maximum = size; // see if current size is a max
 
-
-			if(pos[sfs[i]].empty()) {
-				cur_max = 
-
-			pos[sfs[i]].insert(sfs[i]);
+				beginning = last_pos[sf[i]] +1; // continue for right after the repetition
+				size = i - last_pos[sf[i]]; // update size
+			}
 		}
-
+		cout << maximum << endl;
+	}
 }
 
