3aeba31f71213b0ffac3d00ecdc22a9488206a6e
mrmurilo75 <mrmurilo75@gmail.com>
2021-10-27 18:10:49 +0100

    ps01 - uva 11572 - in progress

---
+++ ps01/uva_11572.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main(){
+	int cases;
+	cin >> cases;
+	while(cases--) {
+		int n;
+		cin >> n;
+		int sf[n], beginning = 0, size = 1, maximum = 1;
+		map<int, int> last_pos;
+		cin >> sf[0];
+		last_pos[sf[0]] = 0;
+		for(int i = 1; i < n; i++) {
+			cin >> sf[i];
+			if(!last_pos[sf[i]]) {
+				last_pos[sf[i]] = i;
+				size++;
+			} else {
+
+
+			if(pos[sfs[i]].empty()) {
+				cur_max = 
+
+			pos[sfs[i]].insert(sfs[i]);
+		}
+
+}
+
