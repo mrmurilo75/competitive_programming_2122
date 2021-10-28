8d78ac2b858a4008229c36e2cc0e65e0923a30de
Murilo Rosa <mrmurilo75@gmail.com>
2021-10-28 22:12:02 +0100

    ps01 - uva 978 - done

---
+++ ps01/uva_978.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+void battle(short green, short blue, pair<short, short> &result) {
+	if(green > blue) {
+		result.first = 1;
+		result.second = green - blue;
+		return;
+	} 
+	if(blue > green) {
+		result.first = 2;
+		result.second = blue - green;
+		return;
+	} 
+	result.first = 0;
+}
+
+int main(){
+	int tests;
+	cin >> tests;
+	while(tests--) { 
+		int bfs, size_g, size_b;
+		cin >> bfs >> size_g >> size_b;
+		priority_queue<short> green_powers, blue_powers;
+		while(size_g--) {
+			short gp;
+			cin >> gp;
+			green_powers.emplace(gp);
+		}
+		while(size_b--) {
+			short bp;
+			cin >> bp;
+			blue_powers.emplace(bp);
+		}
+		while( !(blue_powers.empty() || green_powers.empty()) ) { 
+			int min = (blue_powers.size() < green_powers.size())? blue_powers.size() : green_powers.size(); 
+			if(bfs > min) bfs = min;
+			pair<short, short> results[bfs];
+			for(int i = 0; i < bfs; i++) {
+				battle(green_powers.top(), blue_powers.top(), results[i]);
+				green_powers.pop(); blue_powers.pop();
+			}
+			for(int i = 0; i < bfs; i++) {
+				if(results[i].first == 1)
+					green_powers.push(results[i].second);
+				else if (results[i].first == 2)
+					blue_powers.push(results[i].second);
+				// else both died
+			}
+		}
+		if(blue_powers.empty()) {
+			if(green_powers.empty())
+				cout << "green and blue died\n";
+			else {
+				cout << "green wins\n";
+				while( !(green_powers.empty()) ) {
+					cout << green_powers.top() << "\n";
+					green_powers.pop();
+				}
+			}
+		} else {
+			cout << "blue wins\n";
+			while( !(blue_powers.empty()) ) {
+				cout << blue_powers.top() << "\n";
+				blue_powers.pop();
+			}
+		}
+		if(tests)
+			cout << endl;
+	}
+
+}
+
