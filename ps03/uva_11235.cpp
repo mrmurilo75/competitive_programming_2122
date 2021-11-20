69854691e237e4c6446a11f565fadfa13537b23c
Murilo Rosa <mrmurilo75@gmail.com>
2021-11-20 20:56:18 +0000

    ps03 - uva 11235 - TLE

--- ps03/uva_11235.cpp
+++ ps03/uva_11235.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 int max(int a, int b) {
-	return (a > b) a : b;
+	return (a > b)? a : b;
 }
 
 int main(){
 	int n, q;
 	for(cin >> n >> q; n != 0 && cin.good(); cin >> n >> q){
-		cout << "n " << n << " q " << q << endl;
+		//cout << "n " << n << " q " << q << endl;
 
 		vector<int> values(n);
 		unordered_map< int, pair<int, int> > map;
@@ -20,11 +20,11 @@ int main(){
 		int in, begin = 0, end = 0;
 		cin >> in;
 		values[end] = in;
-		cout << " " << values[end];
+		//cout << " " << values[end];
 		for(end =1; end < n; end++) {
 			cin >> in;
 			values[end] = in;
-			cout << " " << values[end];
+			//cout << " " << values[end];
 
 			if(values[end] != values[end-1]) {
 				map.emplace(values[end-1], pair<int, int>(begin, end));
@@ -32,28 +32,33 @@ int main(){
 			}
 		}
 		map.emplace(values[end-1], pair<int, int>(begin, end));
+		//cout<<endl;
 
 		while(q--) {
 			int i, j;
 			cin >> i >> j;
-			cout << "i " << i << " j " << j << endl;
+			//cout << "i " << i << " j " << j << endl;
 
-			int val, val_end, count = 0;
+			int val, val_end = i, val_begin = i, count = 0;
 			while(val_end < j) {
-				// check the value in i
-				val = values[i];
-				auto val_range = *(map.find(values));
-				val_end = val_range->second;
+				// check the value in i (for first)
+				val = values[val_begin];
+				auto val_range = map.find(val)->second; 	// pair<begin, end> (or <i, end> for first)
+				val_end = val_range.second;
 				
 				if(val_end > j)
 					val_end = j;
+				//cout << "[" << val_begin << " " << val_end << "] " << val_end - val_begin << endl;
 
-				count = max(val_end - i, count);
+				count = max(val_end - val_begin, count);
+
+				val_begin = val_end;
 
 				// 	count = i - value(end)
 				// repeat for value in value(end) as count2
 				// count = max(count, count2)
 			}
+			cout << count << endl;
 		}
 	}
 
