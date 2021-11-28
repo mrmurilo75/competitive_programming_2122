3b902877bf43e20c5ce1768aa8b0a4e8a4bc6ea8
Murilo Rosa <mrmurilo75@gmail.com>
2021-11-28 21:43:47 +0000

    ps04 - spoj mse06h - done (problem was scanf/cin)

--- ps04/spoj_mse06h.cpp
+++ ps04/spoj_mse06h.cpp
@@ -1,86 +1,65 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
-struct FenwickTree { // Fenwick Tree based on https://cp-algorithms.com/data_structures/fenwick.html#toc-tgt-9
-    vector<int> bit;  // binary indexed tree
-    int n;
+struct route {
+	int east, west;
+};
 
-    FenwickTree(int n) {
-        this->n = n + 1;
-        bit.assign(n + 1, 0);
-    }
+int bit[1009];
 
-    int sum(int idx) {
-        int ret = 0;
-        for (++idx; idx > 0 && idx < n; idx -= idx & -idx)
-            ret += bit[idx];
-        return ret;
-    }
-
-    int sum(int l, int r) {
-        return sum(r) - sum(l - 1);
-    }
-
-    void add(int idx, int delta) {
-        for (++idx; idx < n; idx += idx & -idx)
-            bit[idx] += delta;
-    }
-    
-    void print() {
-	for( int i : bit ) cout << " " << i;
-	cout << endl;
-    }
-};  // */
-
-long long connections(FenwickTree **connects, const int c_size, int e, int w) {
+long long sum(int pos) {
 	long long result = 0;
-	if(e < c_size-1) 
-		for(int upE = e+1; upE < c_size; upE++) {
-			result += connects[upE]->sum(0, w-1);
-		}
 
-	if(e > 0)
-		for(int downE = e-1; downE >= 0; downE--) {
-			result += connects[downE]->sum(w+1, c_size);
-		}
-
-	return result; 
-}
-
-int main(){
-	int casesT;
-	cin >> casesT;
-	//cout << "casesT " << casesT << endl;
-
-	for(int i=1; i <= casesT; i++){
-		int eastN, westM, waysK, result = 0;
-		cin >> eastN >> westM >> waysK;
-		//cout <<"N "<< eastN <<"\tM "<< westM <<"\tK "<< waysK << endl;
-
-		FenwickTree **connects = (FenwickTree**) malloc(eastN * sizeof(FenwickTree*));
-		for(int i=0; i < eastN; i++) {
-			connects[i] = new FenwickTree(westM);
-//			connects[i]->print();
-		}
-
-		for(int i=0, e, w; i < waysK; i++) {
-			cin >> e >> w;
-			e--, w--;
-			//cout <<"e "<< e <<"\tw "<< w <<endl;
-
-			connects[e]->add(w, 1);
-//			connects[e]->print();
-			result += connections(connects, eastN, e, w);
-
-			//cout << "r " << result << endl;
-		}
-
-		cout << "Test case "<< i <<": " << result << endl;
-
+	while(pos) {
+		result += bit[pos];
+		pos -= (pos & -pos);
 	}
 
+	return result;
+}
+
+void increase(int pos, int bound) {
+	while(pos <= bound) {
+		bit[pos]+=1;
+		pos += (pos & -pos);
+	}
+}
+
+bool compare(const route &a ,const route &b) {
+	return a.east == b.east ? a.west < b.west : a.east < b.east;
+}
+
+int main() {
+	int t;
+	scanf("%d", &t);
+
+	for(int l = 1; l <= t; l++) {
+		int n, m, k, a, b;
+		scanf("%d%d%d", &n, &m, &k);
+
+		route ways[1000009];
+		memset(bit,0,sizeof bit);
+
+		for(int i = 0; i < k; i++) {
+			scanf("%d%d", &a, &b);
+
+			ways[i].east = a;
+			ways[i].west = b;
+		}
+
+		sort(ways,ways+k,compare);
+
+		long long result = 0;
+		for(int i = 0; i < k; i++) {
+			result += (sum(m) - sum(ways[i].west));
+			increase(ways[i].west , m);
+		}
+
+		cout << "Test case " << l << ": " << result << endl;
+	}
 }
 
