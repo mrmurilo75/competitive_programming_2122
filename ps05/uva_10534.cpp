c3fbab4a0813a1ad58e65adf84c93f4a55c4eb5c
Murilo Rosa <mrmurilo75@gmail.com>
2021-12-01 19:36:26 +0000

    ps05 - uva 10534 - WA (working on all uDebug inputs)

--- ps05/uva_10534.cpp
+++ ps05/uva_10534.cpp
@@ -1,70 +1,96 @@
 #include <iostream>
 #include <cstring>
 #include <map>
+#include <limits.h>
 
-#define ll long long
 #define MAX 10009
 
-#define ABOVE 1
-#define BELOW 2
+#define ABOVE 0
+#define BELLOW 1
 
 #define max(a, b) ((a < b)? b : a)
+#define min(a, b) ((a < b)? a : b)
 #define key(a) (*a).first
 #define value(a) (*a).second
 
 using namespace std;
 
 
-int og[MAX], size;
-long long result =0, waivo =0;
+long long og[MAX];
+int sizeN, count[MAX][2];
 
 int main() {
 
-	while(!cin.eof()) {
-		cin >> size;
-		//cout << size << endl;
+	for(cin >> sizeN; !cin.eof(); cin >> sizeN) {
+		//cout <<"\n----------\n"<< sizeN << endl;
 
-		map<int, long long> above, bellow;
+		int result =0, waivo =0;
+
+		map<long long, int> above, bellow;
 		bellow.emplace(0,0);
 		above.emplace(0,0);
-		bellow.emplace(MAX,0);
-		above.emplace(MAX,0);
+		bellow.emplace(LLONG_MAX,0);
+		above.emplace(LLONG_MAX,0);
 
-		for(int i=0; i<size; i++) {
+		//cout << "bellow: " << endl;
+		for(int i=0; i<sizeN; i++) {
 			cin >> og[i];
 			//cout << " " << og[i] << "\t";
 
-			auto floor = bellow.lower_bound(og[i]);
-			//cout << key(floor) << ", " << value(floor) << endl;
-			if( key(floor) == og[i] ) // (*floor).first 
-				continue;
-			else {
-				--floor;
-			//cout << key(floor) << ", " << value(floor) << endl;
-				bellow.emplace_hint(floor, og[i], value(floor) +1); // (*floor).second
+			auto floor = bellow.lower_bound(og[i]), cur = floor, next = floor;
+			--floor;
+			count[i][BELLOW] = value(floor)+1;
+			if( key(cur) == og[i] ) {
+				next++;
+				//cout << "e " << key(cur) <<","<< value(cur) <<"\n\t";
+				bellow.erase(cur);
 			}
+			if( value(next) <= count[i][BELLOW] && value(next) ) {
+				//cout << "e " << key(next) <<","<< value(next) <<"\n\t";
+				bellow.erase(next);
+			}
+			//cout << key(floor) << ", " << value(floor) << "\t";
+			//cout << og[i] << ", " << count[i][BELLOW];
+			bellow.emplace_hint(floor, og[i], count[i][BELLOW]); // (*floor).second
+			//cout  << endl;
 		}
 		//cout << endl;
-		for(int i=size-1; i>=0; i--) {
+		//cout << "above" << endl;
+		for(int i=sizeN-1; i>=0; i--) {
 			//cout << " " << og[i] << "\t";
-			auto ceiling = above.lower_bound(og[i]), hint = ceiling, floor = bellow.lower_bound(og[i]);
-			//cout << key(ceiling) << ", " << value(ceiling) << "\t";
-			if( key(ceiling) == og[i] )
-				continue;
-			else {
-				--hint;
-				//cout << key(hint) << ", " << value(hint);
-				above.emplace_hint(hint, og[i], value(ceiling) +1);
 
-				waivo = max(value(ceiling)+1, value(floor));
-				result = max(result, waivo);
+			auto floor = above.lower_bound(og[i]), cur = floor, next = floor;
+			--floor;
+			count[i][ABOVE] = value(floor)+1;
+			if( key(cur) == og[i] ) {
+				next++;
+				//cout << "e " << key(cur) <<","<< value(cur) <<"\n\t";
+				above.erase(cur);
 			}
-			//cout << endl;
+			if( value(next) <= count[i][ABOVE] && value(next) ) {
+				//cout << "e " << key(next) <<","<< value(next) <<"\n\t";
+				above.erase(next);
+			}
+			//cout << key(floor) << ", " << value(floor) << "\t";
+			//cout << og[i] << ", " << count[i][ABOVE];
+			above.emplace_hint(floor, og[i], count[i][ABOVE]); // (*floor).second
+			//cout  << endl;
+		}
+
+		//cout << "\nres" << endl;
+		for(int i=0; i<sizeN; i++) {
+			auto ceiling = count[i][ABOVE], floor = count[i][BELLOW];
+			//cout << og[i] <<"\t"<< floor <<"\t"<< ceiling << "\t=> ";
+
+			waivo = min(ceiling, floor);
+			//cout << waivo << " ==> ";
+			// result = max(result, waivo + ((waivo %2)? 0:1) );
+			result = max(result, (2* waivo -1) );
+			//cout << result << endl;
 		}
 
 		cout << result << endl;
-
 	}
-
+	cout << endl;
 }
 
