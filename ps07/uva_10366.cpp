86be0d5a81348a5dbcdc355265d2df936d0dc5a5
Murilo Rosa <mrmurilo75@gmail.com>
2022-01-06 20:37:39 +0000

    ps07 - uva 10366 - in progress (working on example but WA)

--- ps07/uva_10366.cpp
+++ ps07/uva_10366.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <utility>
 #include <stack>
+#include <vector>
 #include <cstring>
 #include <climits>
 
@@ -23,14 +24,15 @@ int main() {
     scanf("%d", &N);
     for(int n=1; n<=N; n++) {
         scanf("%d%d ", &H, &W);
-        cout<<H<<" "<<W<<endl;
+        // cout<<H<<" "<<W<<endl;
 
         for(int i=1; i<=H; i++) {
             for(int j=1; j<=W; j++) {
                 cin >> states[i][j];
-                cout << states[i][j] << " ";
+
+                // cout << states[i][j] << " ";
             }
-            cout << endl;
+            // cout << endl;
         }
 
         vector< pair<int, char> > hash_count(26);
@@ -39,10 +41,14 @@ int main() {
 
 
         stack< pair<int, int> > stac;
+
+        // printf("i j \tstate\tvisited \n");
         for(int i=1; i<=H; i++) {
-            for (int j = 1; j <= W; j++) {
+            for (int j=1; j<=W; j++) {
+                // printf("ff %d %d \t%c\t%d\n", i, j, states[i][j], visited[i][j]);
+
                 if (states[i][j] && !visited[i][j]) {
-                    (hash_count[ 'a' -states[i][j] ]).second ++;
+                    (hash_count[ states[i][j] -'a' ]).second += 1;
 
                     stac.emplace(i, j);
                     visited[i][j] = true;
@@ -56,7 +62,7 @@ int main() {
 
                             if(states[a][b] && 
                                     !visited[a][b] && 
-                                    states[a][b] == states[p.first][b]) {
+                                    states[a][b] == states[p.first][p.second]) {
 
                                 visited[a][b] = true;
                                 stac.emplace(a, b);
@@ -67,7 +73,7 @@ int main() {
 
                             if(states[a][b] && 
                                     !visited[a][b] && 
-                                    states[a][b] == states[p.first][b]) {
+                                    states[a][b] == states[p.first][p.second]) {
 
                                 visited[a][b] = true;
                                 stac.emplace(a, b);
@@ -84,11 +90,15 @@ int main() {
 
         sort(hash_count.begin(), hash_count.end(), sort_states);
 
-        for(pair<int, int> p : hash_count) {
-            if(!p.second)
+        // printf("%d\n", hash_count.size());
+
+        for(int i=0; i<26; i++) {
+            pair< int, int > p = hash_count[i];
+
+            if(p.second == 0)
                 break;
 
-            //printf("%c: %d", p.first, p.second);
+            printf("%c: %d\n", p.first, p.second);
         }
 
     }
