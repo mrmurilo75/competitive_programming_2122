d4cbc1b0849e4f1703ed7dd9131b989cfd383e50
Murilo Rosa <mrmurilo75@gmail.com>
2022-01-12 14:43:47 +0000

    match contest compiler options

--- test_cpp.sh
+++ test_cpp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ls | grep 'exp_out.txt'; then 
-	g++ -Wall $1 -o a.o && ./a.o < 'in.txt' > 'out.txt' 
+	g++ -Wall -O1 -std=gnu++14 $1 -o a.o && ./a.o < 'in.txt' > 'out.txt' 
 	vimdiff 'exp_out.txt' 'out.txt' 
 else
 	g++ -Wall $1 -o a.o && ./a.o < 'in.txt' 
