b41aaa12a454f4393cd20bd658e8646265289a6d
Murilo Rosa <mrmurilo75@gmail.com>
2022-01-15 00:41:07 +0000

    / - test cpp - use c11

--- test_cpp.sh
+++ test_cpp.sh
@@ -2,7 +2,7 @@
 
 cat $1 | xclip -sel clip
 if ls | grep 'exp_out.txt'; then 
-	g++ -Wall -O1 -std=gnu++14 $1 -o a.o && ./a.o < 'in.txt' > 'out.txt' \
+	g++ -Wall -O2 -std=gnu++11 $1 -o a.o && ./a.o < 'in.txt' > 'out.txt' \
     && cat 'out.txt' \
 	&& vimdiff 'exp_out.txt' 'out.txt' 
 else
