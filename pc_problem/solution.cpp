0cc8beff983db448a2ddf4fe9aa62d49bbc0e98b
Murilo Rosa <mrmurilo75@gmail.com>
2022-01-31 00:09:02 +0000

    pc problem - solution cpp - continue

--- pc_problem/solution.cpp
+++ pc_problem/solution.cpp
@@ -4,16 +4,19 @@
 #include <utility>
 #include <vector>
 #include <tuple>
-#include <set>
 
 #define DBG true
 #define dbg if(DBG)cout
 
 using namespace std;
 
-int T, N, M, A, B, W;
+int T, N, M, A, B, W, max_b;
 vector< tuple<int, int, int> > edges;
-vector< set< pair<int, int> > > distances;
+vector< vector< pair<int, int> > > distances;
+
+bool compare(pair< int, int > & a, pair< int, int > & b) {
+    return (a.first != b.first)? a.first < b.first : a.second > b.second;
+}
 
 int main(){
 
@@ -22,6 +25,7 @@ int main(){
     while(T--) {
         edges.clear();
         distances.clear();
+        max_b = 0;
 
         cin >> N;
         dbg <<"N "<< N << endl;
@@ -32,18 +36,27 @@ int main(){
             dbg << A <<"\t"<< W <<"\t"<< B <<endl;
 
             edges.emplace_back(A, W, B);
+            max_b = max(max_b, B);
         }
-
         cin >> M;
         dbg <<"M "<< M <<endl;
 
+        dbg <<"max B " << max_b <<endl;
+        for(int b=0; b<max_b; b++) {
+            distances.emplace_back();
+            distances[b].emplace_back(INT_MAX, INT_MAX);
+        }
+
+        // Bellman-ford but
+        //      for each of the possible paths going thru a negative path you need to check if it is within limit of 
         /*
-        distances[x] = 0;
-        for (int i = 1; i <= n-1; i++) {
-            for (auto e : edges) {
-                int a, b, w;
-                tie(a, b, w) = e;
-                distances[b] = min(distances[b], distances[a]+w);
+        distances[0][0] = make_pair(0, 0);
+        for(int n=0; n<N; n++) {
+            for(auto e : edges) {
+                tie(A, W, B) = e;
+                int jumps = distance[A -1]
+                if(W > 0)
+                    distances[b](distances[a]+w);
             }
         }
         // */
