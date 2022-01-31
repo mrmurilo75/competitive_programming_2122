bec27ea8874eb4dd530940b77a58519afc4bff9f
Murilo Rosa <mrmurilo75@gmail.com>
2022-01-31 00:27:55 +0000

    pc problem - solution2 cpp - continue

--- pc_problem/solution2.cpp
+++ pc_problem/solution2.cpp
@@ -10,22 +10,15 @@
 
 using namespace std;
 
-int T, N, M, A, B, W, max_b;
+int T, N, A, B, W;
 vector< tuple<int, int, int> > edges;
 vector< int > distances;
-
-bool compare(pair< int, int > & a, pair< int, int > & b) {
-    return (a.first != b.first)? a.first < b.first : a.second > b.second;
-}
 
 int main(){
 
     cin >> T;
     dbg <<"T "<< T <<endl;
     while(T--) {
-        edges.clear();
-        distances.clear();
-        max_b = 0;
 
         cin >> N;
         dbg <<"N "<< N << endl;
@@ -36,15 +29,6 @@ int main(){
             dbg << A <<"\t"<< W <<"\t"<< B <<endl;
 
             edges.emplace_back(A, W, B);
-            max_b = max(max_b, B);
-        }
-        cin >> M;
-        dbg <<"M "<< M <<endl;
-
-        dbg <<"max B " << max_b <<endl;
-        for(int b=0; b<max_b; b++) {
-            distances.emplace_back();
-            distances[b].emplace_back(INT_MAX, INT_MAX);
         }
 
         /*
@@ -52,7 +36,7 @@ int main(){
         for(int n=0; n<N; n++) {
             for(auto e : edges) {
                 tie(A, W, B) = e;
-                distances[b] = min(distance[b], distances[a]+w);
+                distances[B] = min(distance[B], distances[A]+W);
             }
         }
         // */
